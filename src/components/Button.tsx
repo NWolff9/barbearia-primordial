86e91796e5f6a25cf66d0138fda9fa19591aae17
Nathã Wolff <natha.wolff9@gmail.com>
2023-03-27 14:24:37 +0000

    create components

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+
+const Button = () => {
+  return (
+    <button
+      
+      className="bg-blue-500 text-white font-bold px-5 py-2 rounded-sm hover:bg-blue-400">
+    
+    </button>
+  );
+}
+
+export default Button;
